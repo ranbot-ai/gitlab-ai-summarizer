ceb39698006f74a3c082410f8624a3f21861087b
Encore Shao <encore.shao@gmail.com>
2024-09-29 20:57:07 +0800

    feat: able to open/close ranbot ext

--- src/utils/extView.ts
+++ src/utils/extView.ts
@@ -1,4 +1,8 @@
 /* eslint-disable eqeqeq */
+
+import { getThemeColor } from ".";
+const themeColor = await getThemeColor();
+
 /* eslint-disable import/no-anonymous-default-export */
 const defaultTitle = "GitLab AI Summarizer";
 const defaultLogoURL = "https://ranbot.online/assets/img/icon48.png";
@@ -20,9 +24,27 @@ const createExtView = (uri: string) => {
     toggleExtView();
   });
 
+  const headerCloseIcon = document.createElement("span");
+  headerCloseIcon.id = "gitlabAISummarizerHeaderCloseIcon";
+  headerCloseIcon.innerHTML = `
+    <svg fill="#000000" height="34px" width="34px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 330.002 330.002" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.660004"></g><g id="SVGRepo_iconCarrier"> <path id="XMLID_103_" d="M233.252,155.997L120.752,6.001c-4.972-6.628-14.372-7.97-21-3c-6.628,4.971-7.971,14.373-3,21 l105.75,140.997L96.752,306.001c-4.971,6.627-3.627,16.03,3,21c2.698,2.024,5.856,3.001,8.988,3.001 c4.561,0,9.065-2.072,12.012-6.001l112.5-150.004C237.252,168.664,237.252,161.33,233.252,155.997z"></path> </g></svg>`;
+
+  headerCloseIcon.addEventListener("click", () => {
+    toggleExtView();
+  });
+
+  headerCloseIcon.addEventListener("mouseover", () => {
+    headerCloseIcon.style.backgroundColor = `${themeColor}`;
+  });
+
+  headerCloseIcon.addEventListener("mouseout", () => {
+    headerCloseIcon.style.backgroundColor = `white`;
+  });
+
   document.body.appendChild(root);
   root.appendChild(headerRanbotLogo);
   root.appendChild(iframe);
+  root.appendChild(headerCloseIcon);
 };
 
 const displayExtView = (entryURL: string) => {
